eeb82ec36caa68c22282c13174c56d805a72cc92
vildantursic <vildantursic@hotmail.com>
2015-08-04 13:28:20 +0200

    creating gulpfile for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify');
+
+gulp.task('styles', function() {
+  gulp.src("app/scripts/css/**/*.css")
+  .pipe(concat("main.min.css"))
+  .pipe(gulp.dest("dist/scripts/css"));
+});
 
 gulp.task('default', function() {
   // place code for your default task here
